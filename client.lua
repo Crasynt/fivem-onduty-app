d91bde64b216c19d52f05f0cd3bdb8c868b2b940
Crasynt <alexander.jbblack@gmail.com>
2023-10-13 19:47:18 +1300

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,10 +1,10 @@
 local function AddApp()
     local added, errorMessage = exports["lb-phone"]:AddCustomApp({
         identifier = identifier,
-        name = "FiveM-Police-App-Script",
+        name = "FIVEM-ONDUTY-APP",
         description = "Police Onduty App",
         developer = "Crasy",
-        defaultApp = true, -- Not needed to be downloaded,
+        defaultApp = false, -- Not needed to be downloaded,
         size = 59812, -- OPTIONAL in kb
         images = {"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4U8s2RN2Ia9mLKtzTzbAZJQgLSjxgA9LOyw&usqp=CAU"}, -- Icon Image
         ui = GetCurrentResourceName() .. "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4U8s2RN2Ia9mLKtzTzbAZJQgLSjxgA9LOyw&usqp=CAU", -- Icon Image
