8e08f539f94aed4762741516f33eb2fab170666a
Crasynt <alexander.jbblack@gmail.com>
2023-10-13 20:46:38 +1300

    Update fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'cerulean'
+game 'gta5'
+
+-- Specify the common information for your resource collection
+author 'Crasy'
+description 'FiveM Police Script'
+version '1.0'
+
+
+
+
+-- Add any additional client scripts that should be run when the resource starts (if applicable)
+client_scripts {
+  'client.lua'
+}
+
+
+
+-- Add any additional client scripts that should be run when the resource starts (if applicable)
+files {
+    'ui.html', 
+    'ui.lua'
+    
+  }
