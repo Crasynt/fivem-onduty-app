09eb997e16c896f07fdd4a568056086f5ed8788a
Crasynt <147784516+Crasynt@users.noreply.github.com>
2023-10-14 17:10:08 +1300

    Update fxmanifest.lua

--- vanilla-js/fxmanifest.lua
+++ vanilla-js/fxmanifest.lua
@@ -6,7 +6,13 @@ description "A template for creating apps for the LB Phone."
 author "Crasy"
 
 client_script "client.lua"
-
+icon.png
+ui.lua
+colour.css
+dev.js
+script,js
+styles.css
+ui.html
 files {
     "ui/**/*"
 }
