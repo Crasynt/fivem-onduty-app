de60d5eca2e22bcc651a3469b7a523475b167170
Crasynt <147784516+Crasynt@users.noreply.github.com>
2024-07-15 13:01:39 +1200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,17 +1,18 @@
 fx_version 'cerulean'
 game 'gta5'
 
--- Specify the common information for your resource collection
 author 'Crasy & Politeblock07'
 description 'LB Phone Police Application'
 version '1.0'
 
--- Add any additional client scripts that should be run when the resource starts (if applicable)
 client_scripts {
-  'client.lua'
+    'Client.lua'
 }
 
--- Add any additional files that need to be included in the resource package
 files {
-    'ui.html'
+    'Client.lua',
+    'script.js',
+    'ui.html',
+    'Ui.lua',
+    'fxmanifest.lua'
 }
